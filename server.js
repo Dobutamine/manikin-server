e4b2b638552cea5e36dd224c5990a299d1ad2ca3
dobutamine <tim.antonius@outlook.com>
2022-05-30 22:10:44 +0200

    work in progress

--- server.js
+++ server.js
@@ -1,42 +1,37 @@
+const {
+  connectManikin,
+  getAirwayPatency,
+  setAirwayPatency,
+} = require("./manikin");
+const http = require("http");
+const ip = require("ip");
+const express = require("express");
+const { WebSocket } = require("ws");
 
-const http = require("http")
-const ip = require("ip")
-const express = require("express")
-const { WebSocket } = require("ws")
-const app = express()
-const manikin_server_port = 3000
-const server = http.createServer(app)
-const wss = new WebSocket.Server( { server })
+const app = express();
+const manikin_server_port = 3000;
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+var current_ip = ip.address();
 
-const { connectTeensy } = require("./teensy")
-const { connectManikin } = require("./manikin")
+wss.on("connection", (ws) => {
+  ws.on("message", (json_mes) => {
+    // handle the different messages
+    const message = JSON.parse(json_mes);
+    handleWebsocketCommands(ws, message);
+  });
+  console.log(`New connection accepted!`);
+  ws.send("Hi from the manikin server!");
+});
 
-var current_ip = ip.address()
+handleWebsocketCommands = (ws, message) => {};
 
-wss.on('connection', (ws) => {
-    ws.on('message', (json_mes) => {
-        // handle the different messages
-        const message = JSON.parse(json_mes)
-        handleWebsocketCommands(message)
-        ws.send('OK')
-    })
-    console.log(`New connection accepted!`)
-    ws.send("Hi from the manikin server!")
-})
-
-handleWebsocketCommands = (command) => {
-    console.log(command)
-}
-
-// connect to the Teensy module inside the grey box
-connectTeensy()
-
-// connect to the Manikin 
-connectManikin()
+// connect to the Manikin
+connectManikin();
 
 // Spin up a server
 server.listen(manikin_server_port, () => {
-    console.log(`Manikin webserver listening on address: ${current_ip} port: ${manikin_server_port}`)
-})
-
-
+  console.log(
+    `Manikin webserver listening on address: ${current_ip} port: ${manikin_server_port}`
+  );
+});
